6cc751e2b52b31b148b37889efac7977827bb4f1
nshib <nshib54321@gmail.com>
2024-08-27 00:54:28 +0300

    Setup and run pre-commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 from wiffy_gui.core import start_app
@@ -7,10 +8,10 @@ from wiffy_gui.core import start_app
 
 def create_files_if_not_exist() -> None:
     Path(".env").touch()
-    load_dotenv('.env')
-    if os.getenv('DOWNLOAD_PATH') is None:
-        user_downloads_path = Path('~').expanduser() / 'Downloads'
-        os.environ['DOWNLOAD_PATH'] = str(user_downloads_path) 
+    load_dotenv(".env")
+    if os.getenv("DOWNLOAD_PATH") is None:
+        user_downloads_path = Path("~").expanduser() / "Downloads"
+        os.environ["DOWNLOAD_PATH"] = str(user_downloads_path)
     Path("songs_data.txt").touch()
 
 
